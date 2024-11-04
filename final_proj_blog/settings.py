8a6bc69898e47b3b4982f18bba15391d962c65b7
yooobwoood <jjeonyuu@gmail.com>
2024-11-05 00:15:53 +0900

    Nginx 적용하기

--- final_proj_blog/settings.py
+++ final_proj_blog/settings.py
@@ -142,7 +142,7 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, '_static')
 
 MEDIA_URL = '/media/'
