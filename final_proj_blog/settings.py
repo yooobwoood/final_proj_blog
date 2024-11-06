018939e134547174c4d2a332c5f6af3171fa387e
yooobwoood <jjeonyuu@gmail.com>
2024-11-06 21:46:25 +0900

    아싸

--- final_proj_blog/settings.py
+++ final_proj_blog/settings.py
@@ -118,8 +118,8 @@ DATABASES = {
 # AWS settings
 AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
-AWS_S3_REGION_NAME = os.getenv("AWS_S3_REGION_NAME")
-AWS_S3_BUCKET_NAME = os.getenv("AWS_S3_BUCKET_NAME")
+AWS_S3_REGION_NAME = "ap-northeast-2"
+AWS_S3_BUCKET_NAME = "jeongsoobin-index"
 
 
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.%s.amazonaws.com' % (
--- final_proj_blog/utils.py
+++ final_proj_blog/utils.py
@@ -9,7 +9,7 @@ def download_index_from_s3(file_name, local_path):
         aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
         aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
     )
-    bucket_name = settings.AWS_S3_BUCKET_NAME
+    bucket_name = "jeongsoobin-index"
     s3.download_file(bucket_name, file_name, local_path)
 
 
